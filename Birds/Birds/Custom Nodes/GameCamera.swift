887877ebda8de0520d651999af2d95abd388bb5f
Thomas Cacciatore <tcacciatore597@gmail.com>
2020-06-11 16:50:27 -0400

    Setup camera constraints and Panning.

---
+++ Birds/Birds/Custom Nodes/GameCamera.swift
@@ -0,0 +1,26 @@
+//
+//  GameCamera.swift
+//  Birds
+//
+//  Created by Thomas Cacciatore on 6/11/20.
+//  Copyright © 2020 Thomas Cacciatore. All rights reserved.
+//
+
+import SpriteKit
+
+class GameCamera: SKCameraNode {
+    func setContraints(with scene: SKScene, and frame: CGRect, to node: SKNode?) {
+        let scaledSize = CGSize(width: scene.size.width * xScale, height: scene.size.height * yScale)
+        let boardContentRect = frame
+        
+        let xInset = min(scaledSize.width / 2, boardContentRect.width / 2)
+        let yInset = min(scaledSize.height / 2, boardContentRect.height / 2)
+        
+        let insetContentRect = boardContentRect.insetBy(dx: xInset, dy: yInset)
+        let xRange = SKRange(lowerLimit: insetContentRect.minX, upperLimit: insetContentRect.maxX)
+        let yRange = SKRange(lowerLimit: insetContentRect.minY, upperLimit: insetContentRect.maxY)
+        
+        let levelEdgeContraint = SKConstraint.positionX(xRange, y: yRange)
+        constraints = [levelEdgeContraint]
+    }
+}
--- Birds/Birds/Scenes/GameScene.swift
+++ Birds/Birds/Scenes/GameScene.swift
@@ -11,11 +11,28 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let gameCamera = SKCameraNode()
+    var mapNode = SKTileMapNode()
+    
+    let gameCamera = GameCamera()
+    var panRecognizer = UIPanGestureRecognizer()
     
     override func didMove(to view: SKView) {
-        addCamera()
+        setupLevel()
+        setupGestureRecognizers()
+    }
+    
+    func setupGestureRecognizers() {
+        guard let view = view else { return }
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        view.addGestureRecognizer(panRecognizer)
+    }
+    
+    func setupLevel() {
+        if let mapNode = childNode(withName: "Tile Map Node") as? SKTileMapNode {
+            self.mapNode = mapNode
+        }
         
+        addCamera()
     }
     
     func addCamera() {
@@ -23,6 +40,18 @@ class GameScene: SKScene {
         addChild(gameCamera)
         gameCamera.position = CGPoint(x: view.bounds.size.width/2, y: view.bounds.size.height/2)
         camera = gameCamera
+        gameCamera.setContraints(with: self, and: mapNode.frame, to: nil)
+    }
+    
+}
+
+extension GameScene {
+    
+    @objc func pan(sender: UIPanGestureRecognizer) {
+        guard let view = view else { return }
+        let translation = sender.translation(in: view)
+        gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
     }
     
 }
