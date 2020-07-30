c31f066e3066bba00b7bce35467f7c3231a0add5
Thomas Cacciatore <tcacciatore597@gmail.com>
2020-07-30 18:10:39 -0400

    Added block assets wood, glass, and stone.

--- Birds/Birds/Scenes/GameScene.swift
+++ Birds/Birds/Scenes/GameScene.swift
@@ -109,11 +109,11 @@ class GameScene: SKScene {
                 let block = Block(type: type)
                 block.size = child.size
                 block.position = child.position
-                block.color = UIColor.brown
+                block.zRotation = child.zRotation
                 block.zPosition = ZPosition.obstacles
                 block.createPhysicsBody()
                 mapNode.addChild(block)
-                child.color = UIColor.clear
+                child.removeFromParent()
             }
         }
         
